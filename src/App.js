5f6963be50a1edefed28278316eedaa22d3f7b41
Da3shay <dakshaysachdeva568@gmail.com>
2022-10-08 01:13:47 +0530

    router done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Home from './Home';
+import Admin from './Admin';
+import { BrowserRouter as Router, Routes , Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div >
-<Home/>
+    <Router>
+    <div>
+     
+      {/* 👇️ Wrap your Route components in a Routes component */}
+      <Routes>
+        <Route path="/Admin" element={<Admin/>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </div>
+  </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -2,42 +2,49 @@ import React from "react";
 import "./Home.scss";
 import { Egress, gmail, lock } from "./Assets/image";
 import Admin from "./Admin";
+import { BrowserRouter as Router, Routes , Route, Link } from "react-router-dom";
 
-const login=()=>
-{
-    
-}
+const login = () => {};
+
 export default function Home() {
-   
   return (
-    <div className="home">
-      <div className="login_block">
-        <div className="left_login">
-          <img src={Egress}></img>
-        </div>
 
-        <div className="right_login">
-          <h1 className="Heading">Welcome Back</h1>
-          <div className="miniHeading">
-            Please enter your egress account details
-          </div>
-          <form className="loginform">
-          <div className="field">
-            <div className="email_label">Email ID</div>
-            <input   placeholder="Enter your email address" className="email"/>
-          </div>
-          <div className="field">
-            <div className="pass_label" >Password</div>
-            <input  placeholder="Enter your password" className="password"/>
-          </div>
-          <div className="field">
-           <button className="buttonn" onClick={login}>Log In</button>
+      <div className="home">
+        <div className="login_block">
+          <div className="left_login">
+            <img src={Egress}></img>
           </div>
 
-          </form>
-       
+          <div className="right_login">
+            <h1 className="Heading">Welcome Back</h1>
+            <div className="miniHeading">
+              Please enter your egress account details
+            </div>
+            <form className="loginform">
+              <div className="field">
+                <div className="email_label">Email ID</div>
+                <input
+                  placeholder="Enter your email address"
+                  className="email"
+                />
+              </div>
+              <div className="field">
+                <div className="pass_label">Password</div>
+                <input placeholder="Enter your password" className="password" />
+              </div>
+              <Link to="/Admin">
+                <div className="field">
+                  <button className="buttonn" onClick={login}>
+                    Log In
+                  </button>
+                </div>
+              </Link>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+ 
+   
+ 
   );
 }
