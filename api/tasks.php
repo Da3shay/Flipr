89c489f2bda56d5f5e041d709d19188f54c1a8fa
CoderGaurav01 <gauravchhapliyal2001@gmail.com>
2022-10-09 20:59:30 +0530

    tasks view

--- api/tasks.php
+++ api/tasks.php
@@ -12,18 +12,21 @@ $conn = $objDb->connect();
 $method = $_SERVER['REQUEST_METHOD'];
 switch($method) {
     case "GET":
+        $sql = "SELECT * FROM tasks";
         $path = explode('/', $_SERVER['REQUEST_URI']);
         if(isset($path[4]) && is_numeric($path[4])) {
-            $today=date("Y-m-d");
-            $yesterday=date("Y-m-d", strtotime("-1 days"));
-            $sql2="SELECT SUM(duration), type, date from tasks where emp_id=:emp_id GROUP BY date,type ORDER BY date DESC LIMIT 7";
-            $stmt2 = $conn->prepare($sql2);
-            $stmt2->bindParam(':emp_id', $path[4]);
-           // $stmt2->bindParam(':date', $today);
-            $stmt2->execute();
-            $users2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+            $sql .= " WHERE id = :id";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $path[4]);
+            $stmt->execute();
+            $tasks = $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
-        echo json_encode($users2);
+
+        echo json_encode($tasks);
         break;
         case "POST":
             $user = json_decode( file_get_contents('php://input') );
