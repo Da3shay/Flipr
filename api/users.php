8136c746b90a9740def0f05308cdf2c783cd2f0f
CoderGaurav01 <gauravchhapliyal2001@gmail.com>
2022-10-09 17:39:57 +0530

    tasks api

--- api/users.php
+++ api/users.php
@@ -14,10 +14,10 @@ switch($method) {
     case "GET":
         $sql = "SELECT * FROM users";
         $path = explode('/', $_SERVER['REQUEST_URI']);
-        if(isset($path[3]) && is_numeric($path[3])) {
+        if(isset($path[4]) && is_numeric($path[4])) {
             $sql .= " WHERE id = :id";
             $stmt = $conn->prepare($sql);
-            $stmt->bindParam(':id', $path[3]);
+            $stmt->bindParam(':id', $path[4]);
             $stmt->execute();
             $users = $stmt->fetch(PDO::FETCH_ASSOC);
         } else {
