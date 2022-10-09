fa95742e4554eb81cf2a30797024eb8a2f3aa9bd
CoderGaurav01 <gauravchhapliyal2001@gmail.com>
2022-10-10 04:15:09 +0530

    task_data api update

--- api/task_data.php
+++ api/task_data.php
@@ -15,7 +15,7 @@ switch($method) {
         $path = explode('/', $_SERVER['REQUEST_URI']);
         if(isset($path[4]) && is_numeric($path[4])) {
             $today=date("Y-m-d");
-            $yesterday=date("Y-m-d", strtotime("-1 days"));
+            $yesterday=date("Y-m-d", strtotime("-2 days"));
             $sql2="SELECT SUM(duration) as time, type, date from tasks where emp_id=:emp_id GROUP BY date,type ORDER BY date DESC LIMIT 7";
             $stmt2 = $conn->prepare($sql2);
             $stmt2->bindParam(':emp_id', $path[4]);
@@ -23,7 +23,23 @@ switch($method) {
             $stmt2->execute();
             $users2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
         }
-        echo json_encode($users2);
+        
+       
+        $result=array();
+        for($i=0;$i<7;$i++){
+            $result[$i]["label"]=date("Y-m-d", strtotime(-$i." days"));;
+            $result[$i]["work"]=0;
+            $result[$i]["break"]=0;
+            $result[$i]["meet"]=0;
+        }
+        
+        foreach($users2 as $key=>$value){
+            $t=strtotime($today);
+            $d=strtotime($users2[$key]['date']);
+            $diff=(($t-$d)/60/60/24);
+            $result[$diff][$users2[$key]['type']]=$users2[$key]['time'];
+        }
+        echo json_encode($result);
         break;
     }
 ?>
